70d51943d02c39e5f145feab469accf69d53900d
HarikrishnanR2001 <84718434+HarikrishnanR2001@users.noreply.github.com>
2021-06-14 18:38:31 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -29,3 +29,51 @@ float average( int a[],int n)
     average+=a[i];
     return average/n;
 } 
+
+/*mode function*/
+int mode(int a[],int n)
+{  
+     int max=0;
+  for(int i=0;i<n;++i)
+   {   
+        if(a[i]>max)
+        max=a[i];
+   }
+   
+     int min=a[0];
+  for(int i=0;i<n;++i)
+   {   
+        if(a[i]<min)
+        min=a[i];
+   } 
+   
+    int c=max-min+1;
+   
+   int count[c];
+   
+   for(int i=0;i<c;++i)
+   
+   {
+       int ctr=0;
+       int x=min+i;
+        for(int j=0;j<n;++j)
+      {
+        if(a[j]==x)
+        ctr+=1;
+      }
+       count[i]=ctr;
+   }
+    
+    int flag=0,m=0;
+    
+    for(int i=0;i<c;++i)
+   {   
+        if(count[i]>m)
+        {m=count[i];
+        flag=i;}
+   }
+  int ret=flag +min;
+   
+   return ret;
+}  
+ 
